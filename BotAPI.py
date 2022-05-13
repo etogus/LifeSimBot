99b9e62cb215102784fa037229f40d5081e2ba17
etogus <mamedov-guseyn@mail.ru>
2022-05-14 00:39:28 +0300

    New version of bot

--- BotAPI.py
+++ BotAPI.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
 
-from main import chatWithBot
+from main import bot
 
 app = Flask(__name__)
 
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route('/chat', methods=['GET', 'POST'])
 def chatBot():
     chatInput = request.form['chatInput']
-    return jsonify(chatBotReply=chatWithBot(chatInput))
+    return jsonify(bot=bot(chatInput))
 
 if __name__ == '__main__':
     app.run(host='192.168.1.55', port=5000, debug=True)
--- main.py
+++ main.py
@@ -1,150 +1,163 @@
-import json
-import pickle
 import random
-
 import nltk
-import numpy
-from nltk.stem import LancasterStemmer
-from tensorflow.python.keras.layers import Dense
-from tensorflow.python.keras.models import Sequential
-from tensorflow.python.keras.models import model_from_yaml
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.linear_model import LogisticRegression
 
-nltk.download('punkt')
+BOT_CONFIG = {
+    'intents': {
+        'hello': {
+            'examples': ['Привет', 'Добрый день', 'Добрый вечер', 'Здравствуйте', 'Шалом', 'Приветики'],
+            'responses': ['Привет, человек!', 'Здравствуйте']
+        },
+        'bye': {
+            'examples': ['Пока', 'Досвидания', 'До свидания', 'Прощайте'],
+            'responses': ['Ещё увидимся', 'Если что, я тут']
+        }
+    },
 
-stemmer = LancasterStemmer()
+    'failure_phrases': [
+        'Непонятно. Перефразируй, пожалуйста',
+        'Я ещё только учусь. Не умею на такое отвечать'
+    ]
+}
 
-with open("intents.json") as file:
-    data = json.load(file)
+X_texts = []  # реплики
+y = []  # их классы
 
-try:
-    with open("chatbot.pickle", "rb") as file:
-        words, labels, training, output = pickle.load(file)
+for intent, intent_data in BOT_CONFIG['intents'].items():
+    for example in intent_data['examples']:
+        X_texts.append(example)
+        y.append(intent)
 
-except:
-    words = []
-    labels = []
-    docs_x = []
-    docs_y = []
-
-    for intent in data["intents"]:
-        for pattern in intent["patterns"]:
-            wrds = nltk.word_tokenize(pattern)
-            words.extend(wrds)
-            docs_x.append(wrds)
-            docs_y.append(intent["tag"])
-
-        if intent["tag"] not in labels:
-            labels.append(intent["tag"])
-
-    words = [stemmer.stem(w.lower()) for w in words if w != "?"]
-    words = sorted(list(set(words)))
-
-    labels = sorted(labels)
-
-    training = []
-    output = []
-
-    output_empty = [0 for _ in range(len(labels))]
-
-    for x, doc in enumerate(docs_x):
-        bag = []
-
-        wrds = [stemmer.stem(w.lower()) for w in doc]
-
-        for w in words:
-            if w in wrds:
-                bag.append(1)
-            else:
-                bag.append(0)
-
-        output_row = output_empty[:]
-        output_row[labels.index(docs_y[x])] = 1
-
-        training.append(bag)
-        output.append(output_row)
-
-    training = numpy.array(training)
-    output = numpy.array(output)
-
-    with open("chatbot.pickle", "wb") as file:
-        pickle.dump((words, labels, training, output), file)
-
-try:
-    yaml_file = open('chatbotmodel.yaml', 'r')
-    loaded_model_yaml = yaml_file.read()
-    yaml_file.close()
-    myChatModel = model_from_yaml(loaded_model_yaml)
-    myChatModel.load_weights("chatbotmodel.h5")
-    print("Loaded model from disk")
-
-except:
-    # Make our neural network
-    myChatModel = Sequential()
-    myChatModel.add(Dense(8, input_shape=[len(words)], activation='relu'))
-    myChatModel.add(Dense(len(labels), activation='softmax'))
-
-    # optimize the model
-    myChatModel.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-
-    # train the model
-    myChatModel.fit(training, output, epochs=1000, batch_size=8)
-
-    # serialize model to yaml and save it to disk
-    model_yaml = myChatModel.to_json()
-    with open("chatbotmodel.yaml", "w") as y_file:
-        y_file.write(model_yaml)
-
-    # serialize weights to HDF5
-    myChatModel.save_weights("chatbotmodel.h5")
-    print("Saved model from disk")
+vectorizer = CountVectorizer()
+X = vectorizer.fit_transform(X_texts)
+clf = LogisticRegression().fit(X, y)
 
 
-def bag_of_words(s, words):
-    bag = [0 for _ in range(len(words))]
+def get_intent(question):
+    question_vector = vectorizer.transform([question])
+    intent = clf.predict(question_vector)[0]
 
-    s_words = nltk.word_tokenize(s)
-    s_words = [stemmer.stem(word.lower()) for word in s_words]
-
-    for se in s_words:
-        for i, w in enumerate(words):
-            if w == se:
-                bag[i] = 1
-
-    return numpy.array(bag)
+    examples = BOT_CONFIG['intents'][intent]['examples']
+    for example in examples:
+        dist = nltk.edit_distance(question, example)
+        dist_percentage = dist / len(example)
+        if dist_percentage < 0.4:
+            return intent
 
 
-def chatWithBot(inputText):
-    currentText = bag_of_words(inputText, words)
-    currentTextArray = [currentText]
-    numpyCurrentText = numpy.array(currentTextArray)
-
-    if numpy.all((numpyCurrentText == 0)):
-        return "I didn't get that, try again"
-
-    result = myChatModel.predict(numpyCurrentText[0:1])
-    result_index = numpy.argmax(result)
-    tag = labels[result_index]
-
-    if result[0][result_index] > 0.7:
-        for tg in data["intents"]:
-            if tg['tag'] == tag:
-                responses = tg['responses']
-
-        return random.choice(responses)
-
-    else:
-        return "I didn't get that, try again"
+def filter_text(text):
+    text = text.lower()
+    text = [c for c in text if c in 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя- ']
+    text = ''.join(text)
+    return text
 
 
-def chat():
-    print("Start talking with the chatbot (try quit to stop)")
-
-    while True:
-        inp = input("You: ")
-        if inp.lower() == "quit":
-            break
-
-        print(chatWithBot(inp))
+def get_answer_by_intent(intent):
+    if intent in BOT_CONFIG['intents']:
+        phrases = BOT_CONFIG['intents'][intent]['responses']
+        return random.choice(phrases)
 
 
-#chat()
+enc = 'utf-8'
+
+with open('dialogues.txt', encoding=enc) as f:
+    content = f.read()
+
+dialogues = [dialogue_line.split('\n') for dialogue_line in content.split('\n\n')]
+
+questions = set()
+qa_dataset = []  # [[q, a]]
+
+for replicas in dialogues:
+    if len(replicas) < 2:
+        continue
+    question, answer = replicas[:2]
+    question = filter_text(question[2:])
+    answer = answer[2:]
+    if question and question not in questions:
+        questions.add(question)
+        qa_dataset.append([question, answer])
+
+qa_by_word_dataset = {}
+for question, answer in qa_dataset:
+    words = question.split(' ')
+    for word in words:
+        if word not in qa_by_word_dataset:
+            qa_by_word_dataset[word] = []
+        qa_by_word_dataset[word].append((question, answer))
+
+qa_by_word_dataset_filtered = {word: qa_list
+                               for word, qa_list in qa_by_word_dataset.items()
+                               if len(qa_list) < 1000}
+
+
+def generate_answer_by_text(text):
+    text = filter_text(text)
+    words = text.split(' ')
+    qa = []
+    for word in words:
+        if word in qa_by_word_dataset_filtered:
+            qa += qa_by_word_dataset[word]
+    qa = list(set(qa))[:2000]
+
+    results = []
+    for question, answer in qa:
+        dist = nltk.edit_distance(question, text)
+        dist_percentage = dist / len(question)
+        results.append([dist_percentage, question, answer])
+
+    if results:
+        dist_percentage, question, answer = min(results, key=lambda pair: pair[0])
+        if dist_percentage < 0.2:
+            return answer
+
+
+def get_failure_phrase():
+    phrases = BOT_CONFIG['failure_phrases']
+    return random.choice(phrases)
+
+
+def bot(question):
+    # NLU
+    intent = get_intent(question)
+
+    # Получение ответа
+
+    # правила
+    if intent:
+        answer = get_answer_by_intent(intent)
+        if answer:
+            return answer
+
+    # генеративная модель
+    answer = generate_answer_by_text(question)
+    if answer:
+        return answer
+
+    # заглушка
+    answer = get_failure_phrase()
+    return answer
+
+
+# def chatWithBot(inputText):
+#     currentText = bag_of_words(inputText, words)
+#     currentTextArray = [currentText]
+#     numpyCurrentText = numpy.array(currentTextArray)
+#
+#     if numpy.all((numpyCurrentText == 0)):
+#         return "I didn't get that, try again"
+#
+#     result = myChatModel.predict(numpyCurrentText[0:1])
+#     result_index = numpy.argmax(result)
+#     tag = labels[result_index]
+#
+#     if result[0][result_index] > 0.7:
+#         for tg in data["intents"]:
+#             if tg['tag'] == tag:
+#                 responses = tg['responses']
+#
+#         return random.choice(responses)
+#
+#     else:
+#         return "I didn't get that, try again"
